334280a7ac3c41c44f849da0ab338e629a772368
atefAkl <atefakl80@gmail.com>
2025-11-12 10:56:18 +0300

    ngrok deployment testing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
+
+    // Trust all proxies for ngrok
+    $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
